897c2692ed24e8f286bb2871b6c5fdee798f449c
Davy Meers <davymeers@hotmail.com>
2013-03-13 23:04:47 +0100

    Refactor controller to improve readability
    Anonymous functions are transformed to named functions.
    
    Signed-off-by: Davy Meers <davymeers@hotmail.com>

--- frontend/js/openAssets.js
+++ frontend/js/openAssets.js
@@ -6,27 +6,41 @@ angular.module('openAssets', ['ngResource']).
   });
 
 function OpenAssetsCtrl($scope, $http) {
-  $http.get('examples/input.json').success(
-    function(data, status, headers, config) {
-      $scope.ep_object_id = data.ep_object_id;
-      $scope.document_id = data.document_id;
-      $scope.docimages = data.images;
-    }
-  );
+  retrieveImageInfo = function(){
+    return $http.get(window.location.pathname + 'examples/input.json');
+  }   
+ 
+  bindImageInfo = function(data, status, headers, config) {
+    $scope.ep_object_id = data.ep_object_id;
+    $scope.document_id = data.document_id;
+    $scope.docimages = data.images;
+  } 
+  
+  retrieveExampleOutput = function(){
+    return $http.get('examples/output.json');
+  }
+  
+  postDocument = function(document){
+    return $http.post(window.location.pathname + 'result', document);
+  }
+  
+  documentUpdateSuccesfull = function(data, status, headers, config) {
+    console.log("Document updated succesfully");
+  }
+  
+  documentUpdateError = function(data, status, headers, config) {
+    console.warn("Document not updated - Statuscode: " + status);
+  }
+  
+  updateDocument =  function(data, status, headers, config) {
+    postDocument(data).success(documentUpdateSuccesfull).error(documentUpdateError);
+  }
   
   $scope.saveInput = function(){
-    //Todo: Store the user-entered data on the server.
-    $http.get(window.location.pathname + 'examples/output.json').success(
-      function(data, status, headers, config) {
-        $http.post(window.location.pathname + 'result', data)
-        .success(function(data, status, headers, config) {
-          console.log("Data posted succesfully");
-        })
-        .error(function(data, status, headers, config) {
-          console.warn("Data not posted - Statuscode: " + status);
-        });
-      }
-    );
+    retrieveExampleOutput().success(updateDocument);
   };
+  
+   retrieveImageInfo().success(bindImageInfo);
 }
- 
+
+
