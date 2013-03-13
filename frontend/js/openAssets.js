227b5d562fa02009b084e98f2076c507a2154dbb
Davy Meers <davymeers@hotmail.com>
2013-03-13 20:22:09 +0100

    Add dummy implementation of saveInput function
    This dummy implementation retrieves the sample output file
    from the server
    and post it to another url.
    This url does not exist and should be changed to the url
    decided by the back-end.
    Also, in the end the data should come from model binding.
    
    Signed-off-by: Davy Meers <davymeers@hotmail.com>

--- frontend/js/openAssets.js
+++ frontend/js/openAssets.js
@@ -16,6 +16,17 @@ function OpenAssetsCtrl($scope, $http) {
   
   $scope.saveInput = function(){
     //Todo: Store the user-entered data on the server.
+    $http.get(window.location.pathname + 'examples/output.json').success(
+      function(data, status, headers, config) {
+        $http.post(window.location.pathname + 'result', data)
+        .success(function(data, status, headers, config) {
+          console.log("Data posted succesfully");
+        })
+        .error(function(data, status, headers, config) {
+          console.warn("Data not posted - Statuscode: " + status);
+        });
+      }
+    );
   };
 }
  
