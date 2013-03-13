ccdc832c42519eeaaf2d6f575d817d21bd79edfa
Piotrek <pit.krakowski@gmail.com>
2013-03-13 19:12:04 +0100

    Projekt

---
+++ backend/openasset/src/main/java/org/hackathon/openassets/model/Document.java
@@ -0,0 +1,15 @@
+package org.hackathon.openassets.model;
+
+public class Document {
+
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
