c711d7e2f8d6ff1634bb180eeb818ce67c7e7ac2
Davy Meers <davymeers@hotmail.com>
2013-03-13 17:01:04 +0100

    Rename controller
    The controller has to do more then only initialzing the forms.
    
    Signed-off-by: Davy Meers <davymeers@hotmail.com>

--- frontend/js/openAssets.js
+++ frontend/js/openAssets.js
@@ -1,11 +1,11 @@
 angular.module('openAssets', ['ngResource']).
   config(function($routeProvider) {
     $routeProvider.
-      when('/', {controller:InitializeForms, templateUrl:'home.html'}).
+      when('/', {controller:OpenAssetsCtrl, templateUrl:'home.html'}).
       otherwise({redirectTo:'/'});
   });
 
-function InitializeForms($scope, $http) {
+function OpenAssetsCtrl($scope, $http) {
   $http.get(window.location.pathname + 'examples/input.json').success(
     function(data, status, headers, config) {
       $scope.docimages = data.images;
