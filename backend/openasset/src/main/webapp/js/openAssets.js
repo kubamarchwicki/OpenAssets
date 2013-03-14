c87fff3289df82e4a9a2739f4b5e9f67a9f6642b
Davy Meers <davymeers@hotmail.com>
2013-03-14 23:54:42 +0100

    Remove dummy code that is no longer needed.
    The userinput is sent to the backend now.
    
    Signed-off-by: Davy Meers <davymeers@hotmail.com>

--- backend/openasset/src/main/webapp/js/openAssets.js
+++ backend/openasset/src/main/webapp/js/openAssets.js
@@ -36,12 +36,8 @@ function OpenAssetsCtrl($scope, $http) {
     console.warn("Document not updated - Statuscode: " + status);
   }
   
-  updateDocument =  function(data, status, headers, config) {
-    postDocument($scope.userinput).success(documentUpdateSuccesfull).error(documentUpdateError);
-  }
-  
   $scope.saveInput = function(){
-    retrieveExampleOutput().success(updateDocument);
+    postDocument($scope.userinput).success(documentUpdateSuccesfull).error(documentUpdateError);
   };
   
    retrieveImageInfo().success(bindImageInfo);
