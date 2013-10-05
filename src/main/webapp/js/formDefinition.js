5c13559a43d9a784c7837915c98c0ffc3f546d36
kubamarchwicki <kuba@marchwicki.pl>
2013-10-05 15:20:00 +0200

    Form keys in polish

--- src/main/webapp/js/formDefinition.js
+++ src/main/webapp/js/formDefinition.js
@@ -3,82 +3,82 @@ app.form_definition = {
 
 		page1: {
 			fields : [
-				{ name: "place_of_birth", desc: "Miejsce urodzenia" },
-				{ name: "work", desc: "Miejsce zatrudnienia"},
-				{ name: "cash_pln", desc: "Środki pieniężne zgromadzone w walucie polskiej" }, 
-				{ name: "cash_other", desc: "Środki pieniężne zgromadzone w walucie obcej" }, 
-				{ name: "stocks", desc: "Papiery wartościowe" },
-				{ name: "house_area", desc: "Dom o powierzchni" },
-				{ name: "house_value", desc: "O wartości" },
-				{ name: "house_ownership", desc: "Tytuł prawny" }
+				{ name: "miejsce_urodzenia", desc: "Miejsce urodzenia" },
+				{ name: "miejsce_zatrudnienia", desc: "Miejsce zatrudnienia"},
+				{ name: "gotowka_pln", desc: "Środki pieniężne zgromadzone w walucie polskiej" }, 
+				{ name: "gotowka_euro", desc: "Środki pieniężne zgromadzone w walucie obcej" }, 
+				{ name: "akcje", desc: "Papiery wartościowe" },
+				{ name: "dom_powierzchnia", desc: "Dom o powierzchni" },
+				{ name: "dom_wartosc", desc: "O wartości" },
+				{ name: "dom_tytul_prawny", desc: "Tytuł prawny" }
 			]
 		},
 		page2: {
 			fields: [
-				{ name: "flat_area", desc: "Mieszkanie	 o powierzchni" },
-				{ name: "flat_value", desc: "O wartości" },
-				{ name: "flat_ownership", desc: "Tytuł prawny" },
-				{ name: "farm_type", desc: "Rodzaj gospodarstwa" },
-				{ name: "farm_area", desc: "Powierzchnia" },
-				{ name: "farm_value", desc: "Wartość" },
-				{ name: "farm_buildings", desc: "Rodzaj zabudowy" },
-				{ name: "farm_ownership", desc: "Tytuł prawny" },
-				{ name: "farm_income", desc: "Przychód i dochód" },
-				{ name: "other_realestate", desc: "Inne nieruchomości" },
-				{ name: "stocks_participation", desc: "Uczestniczę w spółce cywilnej lub osobowej..." },
-				{ name: "stocks_income", desc: "Z tego tytułu osiągnąłem w roku ubiegłym..." },
-				{ name: "stocks_government", desc: "Posiadam udziały w spółce handlowej z udziałem państwowych..." }
+				{ name: "mieszkanie_powierzchnia", desc: "Mieszkanie	 o powierzchni" },
+				{ name: "mieszkanie_wartosc", desc: "O wartości" },
+				{ name: "mieszkanie_tytul_prawny", desc: "Tytuł prawny" },
+				{ name: "gospodarstwo_rodzaj", desc: "Rodzaj gospodarstwa" },
+				{ name: "gospodarstwo_powierzchnia", desc: "Powierzchnia" },
+				{ name: "gospodarstwo_wartosc", desc: "Wartość" },
+				{ name: "gospodarstwo_zabudowa", desc: "Rodzaj zabudowy" },
+				{ name: "gospodarstwo_tytul_prawny", desc: "Tytuł prawny" },
+				{ name: "gospodarstwo_przychod", desc: "Przychód i dochód" },
+				{ name: "nieruchomosci_inne", desc: "Inne nieruchomości" },
+				{ name: "spolki", desc: "Uczestniczę w spółce cywilnej lub osobowej..." },
+				{ name: "spolki_przychod", desc: "Z tego tytułu osiągnąłem w roku ubiegłym..." },
+				{ name: "spolki_panstw", desc: "Posiadam udziały w spółce handlowej z udziałem państwowych..." }
 			]
 			
 		},
 		page3: {
 			fields: [
-				{ name: "stocks_government_more_10_proc", desc: "Udziały te stanowią pakiet większy niż 10% udziałów w spółce:" },
-				{ name: "stocks_government_income", desc: "Z tego tytułu osiągnąłem w roku ubiegłym dochód w wysokości:" },
-				{ name: "stocks_commercial_company", desc: "Posiadam udziały w spółkach handlowych" },
-				{ name: "stocks_commercial_company_income", desc: "Z tego tytułu osiągnąłem w roku ubiegłym dochód w wysokości" },
-				{ name: "stocks_commercial_company_government", desc: "Posiadam akcje w spółkach handlowych" },
-				{ name: "stocks_commercial_company_government_more_10proc", desc: "Akcje te stanowią pakiet większy niż 10% akcji w spółce" },
-				{ name: "stocks_commercial_company_government_income", desc: "Z tego tytułu osiągnąłem w roku ubiegłym dochód w wysokości" },
-				{ name: "stocks_other_company", desc: "Posiadam akcje w innych spółkach handlowych" },
-				{ name: "stocks_other_company_income", desc: "Z tego tytułu osiągnąłem w roku ubiegłym dochód w wysokości" },
-				{ name: "auction", desc: "Nabyłem od Skarbu Państwa, innej państwowej..."}	
+				{ name: "spolki_panstw_10proc", desc: "Udziały te stanowią pakiet większy niż 10% udziałów w spółce:" },
+				{ name: "spolki_panstw_przychod", desc: "Z tego tytułu osiągnąłem w roku ubiegłym dochód w wysokości:" },
+				{ name: "spolki_handlowe", desc: "Posiadam udziały w spółkach handlowych" },
+				{ name: "spolki_handlowe_przychod", desc: "Z tego tytułu osiągnąłem w roku ubiegłym dochód w wysokości" },
+				{ name: "spolki_handlowe_panstw", desc: "Posiadam akcje w spółkach handlowych" },
+				{ name: "spolki_handlowe_panstw_10proc", desc: "Akcje te stanowią pakiet większy niż 10% akcji w spółce" },
+				{ name: "spolki_handlowe_panstw_przychod", desc: "Z tego tytułu osiągnąłem w roku ubiegłym dochód w wysokości" },
+				{ name: "spolki_inne", desc: "Posiadam akcje w innych spółkach handlowych" },
+				{ name: "spolki_inne_przychod", desc: "Z tego tytułu osiągnąłem w roku ubiegłym dochód w wysokości" },
+				{ name: "przetargi", desc: "Nabyłem od Skarbu Państwa, innej państwowej..."}	
 			]
 			
 		},
 		page4: {
 	        fields: [
-	            {name: "self_employment", desc: "Prowadzę działalność gospodarczą (należy podać formę prawną i przedmiot działalności)"},
-	            {name: "self_employment_personal", desc: "osobiście"},
-	            {name: "self_employment_with_others", desc: "wspólnie z innymi osobami"},
-	            {name: "self_employment_income", desc: "Z tego tytułu osiągnąłem (ęłam) w roku ubiegłym przychód i dochód w wysokości"},
-	            {name: "represent_self_employment", desc: "Zarządzam działalnością gospodarczą lub jestem przedstawicielem, pełnomocnikiem takiej działalności (należy podać formę prawną i przedmiot działalności"},
-	            {name: "represent_self_personal", desc: "osobiście"},
-	            {name: "represent_self_with_others", desc: "wspólnie z innymi osobami"},
-	            {name: "represent_self_income", desc: "Z tego tytułu osiągnąłem(ęłam) w roku ubiegłym dochód w wysokości"},
-	            {name: "public_company", desc: "W spółce handlowej (nazwa, siedziba spółki)"},
-	            {name: "public_company_board", desc: "jestem członkiem zarządu (od kiedy)"},
-	            {name: "public_company_directors", desc: "jestem członkiem rady nadzorczje (od kiedy)"},
-	            {name: "public_company_audit", desc: "jestem członkiem komisji rewizyjnej (od kiedy)"},
-	            {name: "public_company_income", desc: "Z tego tytułu osiągnąłem(ęłam) w roku ubiegłym dochód w wysokości"}
+	            {name: "dzialanosc_gosp", desc: "Prowadzę działalność gospodarczą (należy podać formę prawną i przedmiot działalności)"},
+	            {name: "dzialanosc_gosp_osobista", desc: "osobiście"},
+	            {name: "dzialanosc_gosp_zinnymi", desc: "wspólnie z innymi osobami"},
+	            {name: "dzialanosc_gosp_przychod", desc: "Z tego tytułu osiągnąłem (ęłam) w roku ubiegłym przychód i dochód w wysokości"},
+	            {name: "zarzad_dzialanosc_gosp", desc: "Zarządzam działalnością gospodarczą lub jestem przedstawicielem, pełnomocnikiem takiej działalności (należy podać formę prawną i przedmiot działalności"},
+	            {name: "zarzad_dzialanosc_gosp_osobista", desc: "osobiście"},
+	            {name: "zarzad_dzialanosc_gosp_zinnymi", desc: "wspólnie z innymi osobami"},
+	            {name: "zarzad_dzialanosc_gosp_przychod", desc: "Z tego tytułu osiągnąłem(ęłam) w roku ubiegłym dochód w wysokości"},
+	            {name: "spolki_handlowe_rep", desc: "W spółce handlowej (nazwa, siedziba spółki)"},
+	            {name: "spolki_handlowe_rep_zarzad", desc: "jestem członkiem zarządu (od kiedy)"},
+	            {name: "spolki_handlowe_rep_rada", desc: "jestem członkiem rady nadzorczje (od kiedy)"},
+	            {name: "spolki_handlowe_rep_komisja", desc: "jestem członkiem komisji rewizyjnej (od kiedy)"},
+	            {name: "spolki_handlowe_rep_przychod", desc: "Z tego tytułu osiągnąłem(ęłam) w roku ubiegłym dochód w wysokości"}
 	        ]
 	    },
 
 	    page5: {
 	        fields: [
-	            {name: "foundation", desc: "W fundacjach i stowarzyszeniach prowadzących działalność gospodarczą"},
-	            {name: "foundation_board", desc: "jestem członkiem zarządu (od kiedy)"},
-	            {name: "foundation_directors", desc: "jestem członkiem rady nadzorczej (od kiedy)"},
-	            {name: "foundation_audit", desc: "jestem członkiem komisji rewizyjnej (od kiedy)"},
-	            {name: "foundation_income", desc: "Z tego tytułu osiągnąłem(ęłam) w roku ubiegłym dochów w wysokości"},
-	            {name: "other_income", desc: "Inne dochody osiągnięte z tytułu zatrudnienia lub innej działalności zarobkowej lub zajęć, z podaniem kwot uzyskanych z każdego tytułu"},
-	            {name: "cars", desc: "Składniki mienia ruchomego o wartości powyżej 10 000 zł (w przypadku pojazdów mechanicznych należy podać markę, model i rok produkcji)"}
+	            {name: "fundacje", desc: "W fundacjach i stowarzyszeniach prowadzących działalność gospodarczą"},
+	            {name: "fundacje_zarzad", desc: "jestem członkiem zarządu (od kiedy)"},
+	            {name: "fundacje_rada", desc: "jestem członkiem rady nadzorczej (od kiedy)"},
+	            {name: "fundacje_komiscja", desc: "jestem członkiem komisji rewizyjnej (od kiedy)"},
+	            {name: "fundacje_przychod", desc: "Z tego tytułu osiągnąłem(ęłam) w roku ubiegłym dochów w wysokości"},
+	            {name: "inne_przychody", desc: "Inne dochody osiągnięte z tytułu zatrudnienia lub innej działalności zarobkowej lub zajęć, z podaniem kwot uzyskanych z każdego tytułu"},
+	            {name: "samochod", desc: "Składniki mienia ruchomego o wartości powyżej 10 000 zł (w przypadku pojazdów mechanicznych należy podać markę, model i rok produkcji)"}
 	        ]
 	    },
 
 	    page6: {
 	        fields: [
-	            {name: "liabilities", desc: "Zobowiązania pieniężne o wartości powyżej 10 000 zł"}
+	            {name: "zobowiazania", desc: "Zobowiązania pieniężne o wartości powyżej 10 000 zł"}
 	        ]
 	    }
 
